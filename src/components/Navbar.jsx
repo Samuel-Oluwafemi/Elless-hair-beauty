3c720749047223ff76cf98b87326dd9051b75daa
oluwafemix <samuelkeller47@gmail.com>
2025-12-22 22:26:02 +0100

    Few changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export function Navbar() {
   const [open, setOpen] = useState(false);
   return (
     <nav
-      className="fixed top-0 bg-black/30 border-b border-white/20 shadow-[0_10px_30px_rgba(0,0,0,0.3)]
+      className="fixed top-0 bg-black/30 border-b border-white/30 md:border-white/20 shadow-[0_10px_30px_rgba(0,0,0,0.3)]
      backdrop-blur-md md:backdrop-blur-md w-full z-50 py-6 px-3 md:py-5 md:px-5"
     >
       <div className="flex items-center justify-between">
@@ -25,19 +25,19 @@ export function Navbar() {
         {/* Navs right */}
         <ul className="hidden md:flex space-x-9 text-white text-md font-semibold gap-4">
           <a href="#services">
-            <li className="cursor-pointer hover:text-pink-400">Home</li>
+            <li className="cursor-pointer hover:text-pink-500">Home</li>
           </a>
           <a href="#testimonials">
-            <li className="cursor-pointer hover:text-pink-400">Services</li>
+            <li className="cursor-pointer hover:text-pink-500">Services</li>
           </a>
           <a href="">
-            <li className="cursor-pointer hover:text-pink-400">Packages</li>
+            <li className="cursor-pointer hover:text-pink-500">Packages</li>
           </a>
           <a href="#about">
-            <li className="cursor-pointer hover:text-pink-400">Reviews</li>
+            <li className="cursor-pointer hover:text-pink-500">Reviews</li>
           </a>
           <a href="#about">
-            <li className="cursor-pointer hover:text-pink-400">Contact</li>
+            <li className="cursor-pointer hover:text-pink-500">Contact</li>
           </a>
           <a href="#about">
             <li
@@ -58,19 +58,19 @@ export function Navbar() {
       </div>
       {open && (
         <div className="md:hidden flex flex-col text-bold text-xl text-white gap-8 items-center mt-3">
-          <a href="#services" className="cursor-pointer hover:text-pink-400">
+          <a href="#services" className="cursor-pointer hover:text-pink-500">
             Services
           </a>
           <a
             href="#testimonials"
-            className="cursor-pointer hover:text-pink-400"
+            className="cursor-pointer hover:text-pink-500"
           >
             Testimonials
           </a>
-          <a href="" className="cursor-pointer hover:text-pink-400">
+          <a href="" className="cursor-pointer hover:text-pink-500">
             Pricing
           </a>
-          <a href="#about" className="cursor-pointer hover:text-pink-400">
+          <a href="#about" className="cursor-pointer hover:text-pink-500">
             About
           </a>
         </div>

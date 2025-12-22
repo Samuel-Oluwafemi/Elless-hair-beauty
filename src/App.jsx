e144c5f915ecd0713263d876485c155c230dec81
oluwafemix <samuelkeller47@gmail.com>
2025-12-22 22:51:25 +0100

    added a smooth scroll behavior to enable smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Background } from "./components/Background";
 import { Services } from "./services/Services";
 function App() {
   return (
+    <html className="scroll-smooth">
     <div className="min-h-screen bg-white text-white">
       <Navbar />
       <Background />
       <Services />
     </div>
+    </html>
   );
 }
 

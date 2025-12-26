362b654b979c6d305cad80ae964c7d3e894293e4
oluwafemix <samuelkeller47@gmail.com>
2025-12-26 19:03:41 +0100

    Exlcusive packages section

--- src/packages/packages.jsx
+++ src/packages/packages.jsx
@@ -2,7 +2,11 @@ export function Packages() {
   return (
     <>
       <section id="packages" className="py-20 px-4 bg-white">
-        <div className="max-w-7xl mx-auto text-center"></div>
+        <div className="max-w-7xl mx-auto text-center">
+            <h2 className="text-4xl md:text-5xl font-bold text-fuchsia-600 mb-4">
+                Exclusive Packages
+            </h2>
+        </div>
       </section>
     </>
   );

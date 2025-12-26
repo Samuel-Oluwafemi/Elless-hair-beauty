115308a00a7bcfb73046b9142fb8a507c650afeb
oluwafemix <samuelkeller47@gmail.com>
2025-12-26 19:06:11 +0100

    Adding view more collapsible to show and hide services

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -1,4 +1,59 @@
+import { useState } from "react";
 export function Services() {
+  const services = [
+    {
+      id: 1,
+      title: "Wigs",
+      image: "/images/elless wigs.jpeg",
+      description:
+        "Transform your look instantly with our stunning collection of wigs. Whether you desire a new color, length, or style, our wigs offer versatility and convenience.",
+    },
+    {
+      id: 2,
+      title: "Makeup Services",
+      image: "/images/elless makeup.jpeg",
+      description:
+        "Enhance your natural beauty with our professional makeup services. We use only high-quality makeup products to ensure a flawless and long-lasting finish.",
+    },
+    {
+      id: 3,
+      title: "Hair Extensions",
+      image: "/images/elless hair extensions.jpeg",
+      description:
+        "Add length, volume, and dimension to your hair with our premium hair extensions. We offer a variety of extension methods to suit your hair type and lifestyle needs.",
+    },
+    {
+      id: 4,
+      title: "Nail Fixing & Polishing",
+      image: "/images/elless nails.jpeg",
+      description:
+        "Indulge in our luxurious nail care services. Creative and artistic nail designs that express your unique style and personality.",
+    },
+    {
+      id: 5,
+      title: "Tooth Gem Fixing",
+      image: "/images/elless Tooth Gem.jpeg",
+      description:
+        "Add a touch of sparkle to your smile with our tooth gem fixing service. We make your smile shine with our safe and stylish tooth gem application.",
+    },
+    {
+      id: 6,
+      title: "Braiding & Hair Attachment Services",
+      image: "/images/elless braiding2.jpeg",
+      description:
+        "Transform your hair with our expert braiding and hair attachment services. We offer a variety of braiding styles.",
+    },
+    {
+      id: 7,
+      title: "Luxury Pedicure",
+      image: "/images/elless pedicure.jpeg",
+      description:
+        "Experience ultimate relaxation with our luxury pedicure services. Our expert technicians will pamper your feet, leaving them soft, smooth, and beautifully polished.",
+    },
+  ];
+
+  const [expandedService, setExpandedService] = useState(false);
+  const visibleServices = expandedService ? services : services.slice(0, 3);
   return (
     <>
       <section
@@ -17,128 +72,35 @@ export function Services() {
           </p>
           {/* Services section grid */}
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            {/* first column */}
-            <div className="bg-white text-left rounded-lg shadow-lg hover:shadow-fuchsia-400/50 transition-shadow duration-300">
-              <div className=" w-full">
-                <img
-                  src="/images/elless wigs.jpeg"
-                  alt=""
-                  className="w-full h-50 rounded-t-lg object-cover"
-                />
+            {visibleServices.map((service) => (
+              <div
+                key={service.id}
+                className="bg-white text-left rounded-lg hover:scale-105 transition-all transition-transform duration-300 shadow-lg 
+                   hover:shadow-fuchsia-400/50 transition-shadow duration-300"
+              >
+                <div className="w-full">
+                  <img
+                    src={service.image}
+                    alt={service.title}
+                    className="w-full h-50 relative group-hover:scale-105 transition-transform duration-300 rounded-t-lg object-cover"
+                  />
+                </div>
+                <div className="p-6">
+                  <h3 className="text-2xl font-semibold mb-4 text-fuchsia-500">
+                    {service.title}
+                  </h3>
+                  <p className="text-gray-700">{service.description}</p>
+                </div>
               </div>
-              <div className="p-6">
-                <h3 className="text-2xl font-semibold mb-4 text-fuchsia-500">
-                  Wigs
-                </h3>
-                <p className="text-gray-700">
-                  Transform your look instantly with our stunning collection of
-                  wigs. Whether you desire a new color, length, or style, our
-                  wigs offer versatility and convenience.
-                </p>
-              </div>
-            </div>
-            {/* Second column */}
-            <div className="bg-white text-left rounded-lg shadow-lg hover:shadow-fuchsia-400/50 transition-shadow duration-300">
-              <div className=" w-full">
-                <img
-                  src="/images/elless makeup.jpeg"
-                  alt=""
-                  className="w-full h-50 rounded-t-lg object-cover"
-                />
-              </div>
-              <div className="p-6">
-                <h3 className="text-2xl font-semibold mb-4 text-fuchsia-500">
-                  Makeup Services
-                </h3>
-                <p className="text-gray-700">
-                  Enhance your natural beauty with our professional makeup
-                  services. We use only high-quality makeup products to ensure a
-                  flawless and long-lasting finish.
-                </p>
-              </div>
-            </div>
-            {/* Third column */}
-            <div className="bg-white text-left rounded-lg shadow-lg hover:shadow-fuchsia-400/50 transition-shadow duration-300">
-              <div className=" w-full">
-                <img
-                  src="/images/elless hair extensions.jpeg"
-                  alt=""
-                  className="w-full h-50 rounded-t-lg object-cover"
-                />
-              </div>
-              <div className="p-6">
-                <h3 className="text-2xl font-semibold mb-4 text-fuchsia-500">
-                  Hair Extensions
-                </h3>
-                <p className="text-gray-700">
-                  Add length, volume, and dimension to your hair with our
-                  premium hair extensions. We offer a variety of extension
-                  methods to suit your hair type and lifestyle needs.
-                </p>
-              </div>
-            </div>
-            {/* Fourth column */}
-            <div className="bg-white text-left rounded-lg shadow-lg hover:shadow-fuchsia-400/50 transition-shadow duration-300">
-              <div className=" w-full">
-                <img
-                  src="/images/elless nails.jpeg"
-                  alt=""
-                  className="w-full h-50 rounded-t-lg object-cover"
-                />
-              </div>
-              <div className="p-6">
-                <h3 className="text-2xl font-semibold mb-4 text-fuchsia-500">
-                  Nail Fixing & Polishing
-                </h3>
-                <p className="text-gray-700">
-                  Indulge in our luxurious nail care services. Creative and
-                  artistic nail designs that express your unique style and
-                  personality.
-                </p>
-              </div>
-            </div>
-            {/* Fifth column */}
-            <div className="bg-white text-left rounded-lg shadow-lg hover:shadow-fuchsia-400/50 transition-shadow duration-300">
-              <div className=" w-full">
-                <img
-                  src="/images/elless Tooth Gem.jpeg"
-                  alt=""
-                  className="w-full h-50 rounded-t-lg object-cover"
-                />
-              </div>
-              <div className="p-6">
-                <h3 className="text-2xl font-semibold mb-4 text-fuchsia-500">
-                  Tooth Gem Fixing
-                </h3>
-                <p className="text-gray-700">
-                  Add a touch of sparkle to your smile with our tooth gem fixing
-                  service. We make your smile shine with our safe and stylish
-                  tooth gem application.
-                </p>
-              </div>
-            </div>
-            {/* Sixth column */}
-            <div className="bg-white text-left rounded-lg shadow-lg hover:shadow-fuchsia-400/50 transition-shadow duration-300">
-              <div className=" w-full">
-                <img
-                  src="/images/elless braiding2.jpeg"
-                  alt=""
-                  className="w-full h-50 rounded-t-lg object-cover"
-                />
-              </div>
-              <div className="p-6">
-                <h3 className="text-2xl font-semibold mb-4 text-fuchsia-500">
-                  Braiding & Hair Attachment Services
-                </h3>
-                <p className="text-gray-700">
-                  Add a touch of sparkle to your smile with our tooth gem fixing
-                  service. We make your smile shine with our safe and stylish
-                  tooth gem application.
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
+
+        <button type="button" onClick={() =>setExpandedService(prev => !prev)}
+            className="bg-gradient-to-r from-white to-fuchsia-600 text-black rounded-full py-3 px-6 mx-auto text-center
+            flex justify-center mt-10 font-semibold hover:bg-fuchsia-600 hover:text-white cursor-pointer">
+            {expandedService ? 'Show Less' : 'Show More'}
+        </button>
       </section>
     </>
   );
